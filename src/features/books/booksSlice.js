9327fa803662b955d20685ee24ad513cdb71bcdb
RouSage <therousage@gmail.com>
2021-03-17 18:24:30 +0300

    feat: basic redux toolkit setup

---
+++ src/features/books/booksSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const booksSlice = createSlice({
+  name: 'books',
+  initialState: [],
+  reducers: {
+    addBook: (state, action) => {
+      state.push(action.payload);
+    },
+  },
+});
+
+export const { addBook } = booksSlice.actions;
+
+export default booksSlice.reducer;
