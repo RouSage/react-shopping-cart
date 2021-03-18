ebf73104cd937c80d315b29d678041ebd5c8546c
RouSage <therousage@gmail.com>
2021-03-18 19:24:48 +0300

    feat(redux): fetch and display a list of books

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Books from './features/books/Books';
 
 const App = () => (
   <div>
     <h1>Hello World!</h1>
+    <Books />
   </div>
 );
 
--- src/features/books/booksSlice.js
+++ src/features/books/booksSlice.js
@@ -1,15 +1,55 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchBooks = createAsyncThunk(
+  'books/fetchBooks',
+  async (_arg, { getState, requestId }) => {
+    const { currentRequestId, isLoading } = getState().books;
+
+    if (!isLoading || requestId !== currentRequestId) {
+      return false;
+    }
+
+    const response = await axios.get('/books.json');
+    return response.data;
+  }
+);
+
+const initialState = {
+  isLoading: false,
+  items: [],
+  currentRequestId: undefined,
+  error: null,
+};
 
 export const booksSlice = createSlice({
   name: 'books',
-  initialState: [],
-  reducers: {
-    addBook: (state, action) => {
-      state.push(action.payload);
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchBooks.pending]: (state, action) => {
+      if (!state.isLoading) {
+        state.isLoading = true;
+        state.currentRequestId = action.meta.requestId;
+      }
+    },
+    [fetchBooks.fulfilled]: (state, action) => {
+      const { requestId } = action.meta;
+      if (state.isLoading && state.currentRequestId === requestId) {
+        state.isLoading = false;
+        state.items = action.payload;
+        state.currentRequestId = undefined;
+      }
+    },
+    [fetchBooks.rejected]: (state, action) => {
+      const { requestId } = action.meta;
+      if (state.isLoading && state.currentRequestId === requestId) {
+        state.isLoading = false;
+        state.error = action.error;
+        state.currentRequestId = undefined;
+      }
     },
   },
 });
-
-export const { addBook } = booksSlice.actions;
 
 export default booksSlice.reducer;
