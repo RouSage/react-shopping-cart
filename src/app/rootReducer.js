9730f95c4cfaaa81dbfaeca0908cd6e0c3f8b245
RouSage <therousage@gmail.com>
2021-03-19 15:27:59 +0300

    feat(cart): basic cart reducer setup

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import booksReducer from '../features/books/booksSlice';
+import cartReducer from '../features/cart/cartSlice';
 
 const rootReducer = combineReducers({
   books: booksReducer,
+  cart: cartReducer,
 });
 
 export default rootReducer;
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  items: [],
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+    },
+    removeItem: (state, action) => {
+      state.items.filter((item) => item.id !== action.payload);
+    },
+  },
+});
+
+export const { addItem, removeItem } = cartSlice.actions;
+
+export default cartSlice.reducer;
