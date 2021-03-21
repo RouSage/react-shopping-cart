2ebe453ca8c47dcff9f3f832ea4e23ddc5323f7d
RouSage <therousage@gmail.com>
2021-03-21 19:56:28 +0300

    chore: uninstall redux-logger

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 
 export default configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV !== 'production',
 });
